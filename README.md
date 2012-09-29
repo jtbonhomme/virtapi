842fdc6df02b368ac75f3e8222e63b9f1c29e6a8
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2012-09-29 07:37:45 +0200

    to be discussed

---
+++ README.md
@@ -0,0 +1,7 @@
+virtapi
+=======
+
+<pre>
+npm install
+ln -s /path/to/statc/file/directory/to/serve static
+</pre>
--- core/virtapi.py
+++ core/virtapi.py
@@ -1,52 +1,60 @@
 """Basic Libraries"""
 
 import simplejson
+import time
 
-""" WebApp """
+""" WebApp directory (symb. link) """
 
-WEBAPP_ROOT = "/Users/jean-thierrybonhomme/Developments/g7/webapp/public"
-WEBAPP_JS_ROOT = "/Users/jean-thierrybonhomme/Developments/g7/webapp/public"
-WEBAPP_CSS_ROOT = "/Users/jean-thierrybonhomme/Developments/g7/webapp/public"
-DATA_ROOT = "/Users/jean-thierrybonhomme/Developments/virtapi/data"
+DATA_ROOT = "/Users/jean-thierrybonhomme/Developments/virtapi/static"
 
 """ REST API Flask framework """
 
 from flask import Flask, send_from_directory
 app = Flask(__name__)
 
-""" Routes for launching the WebApp """
+""" Routes for communnication with the TestApp """
 
-@app.route('/', methods=['GET'])
-def get_index():
-	return send_from_directory(WEBAPP_ROOT, "index.html")
+@app.route('/event', methods=['GET'])
+def get_event():
+    print "get_event"
+    time.sleep(10)
+    print "wake-up !"
+    return '{"type": "event","params":{"event": "playbackStopped"}}'
+"""    return send_from_directory(DATA_ROOT, 'event.json', mimetype='application/json') """
 
-@app.route('/index.html', methods=['GET'])
-def get_index_html():
-	return send_from_directory("/Users/jean-thierrybonhomme/Developments/g7/webapp/public", "index.html")
+"""
+@app.route('/order/<param>', methods=['POST'])
+def get_order():
+    return ''
+"""
 
-@app.route('/js/g7-webapp.js', methods=['GET'])
-def get_js():
-	return send_from_directory('/Users/jean-thierrybonhomme/Developments/g7/webapp/public/js', 'g7-webapp.js')
+""" Routes for serving the TestApp """
 
-@app.route('/css/g7-webapp.css', methods=['GET'])
+@app.route('/index_testapp.html', methods=['GET'])
+def get_html():
+    return send_from_directory(DATA_ROOT, 'index_testapp.html')
+
+@app.route('/style.css', methods=['GET'])
 def get_css():
-	return send_from_directory('/Users/jean-thierrybonhomme/Developments/g7/webapp/public/css', 'g7-webapp.css')
+    return send_from_directory(DATA_ROOT, 'style.css')
 
-""" Routes for basic calls """
+@app.route('/ajax.js', methods=['GET'])
+def get_ajax():
+    return send_from_directory(DATA_ROOT, 'ajax.js')
 
-@app.route('/channels', methods=['GET'])
-def get_channels():
-	return send_from_directory(DATA_ROOT, 'channels.json')
+@app.route('/longpoll.js', methods=['GET'])
+def get_poll():
+    return send_from_directory(DATA_ROOT, 'longpoll.js')
 
-@app.route('/programs/now', methods=['GET'])
-def get_now():
-	return send_from_directory(DATA_ROOT, 'now.json')
+@app.route('/app_spec.js', methods=['GET'])
+def get_app():
+    return send_from_directory(DATA_ROOT, 'app_spec.js')
 
-@app.route('/programs/next', methods=['GET'])
-def get_next():
-	return send_from_directory(DATA_ROOT, 'next.json')
+@app.route('/CanalLightRomain.otf', methods=['GET'])
+def get_font():
+    return send_from_directory(DATA_ROOT, 'CanalLightRomain.otf')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+    app.run(host='127.0.0.1', port=1234)
 
 
