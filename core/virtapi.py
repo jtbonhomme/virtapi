25ad615b65188754222cdc16382c0650201d3be5
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2012-09-30 15:03:20 +0200

    change longpolling in eventsource

--- core/virtapi.py
+++ core/virtapi.py
@@ -12,21 +12,42 @@ DATA_ROOT = "/Users/jean-thierrybonhomme/Developments/virtapi/static"
 from flask import Flask, send_from_directory
 app = Flask(__name__)
 
-""" Routes for communnication with the TestApp """
+""" SSE implementation """
 
-@app.route('/event', methods=['GET'])
-def get_event():
-    print "get_event"
-    time.sleep(10)
-    print "wake-up !"
-    return '{"type": "event","params":{"event": "playbackStopped"}}'
-"""    return send_from_directory(DATA_ROOT, 'event.json', mimetype='application/json') """
+msgList = []
 
-"""
-@app.route('/order/<param>', methods=['POST'])
-def get_order():
-    return ''
-"""
+def event_stream():
+    for message in msgList:
+        """ update message queue length """
+        get_message.lastLength=len(msgList)
+        yield 'data: %s\n\n' % message
+
+eventStream=event_stream()
+
+def get_message():
+    if not hasattr(get_message, "lastLength"):
+        get_message.lastLength = 0
+    if len(msgList) == get_message.lastLength:
+        """ no message pending (message queue length did not increased), return """
+        return ''
+    else:
+        """ seems a message has been posted, deliver it """
+        return eventStream.next();
+
+@app.route('/post', methods=['POST'])
+def post():
+    message = flask.request.form['message']
+    user = flask.session.get('user', 'anonymous')
+    now = datetime.datetime.now().replace(microsecond=0).time()
+    """ add message to queue """
+    msgList.append('[%s] %s: %s' % (now.isoformat(), user, message))
+    return flask.Response('message posted',
+                          mimetype="text/html")
+@app.route('/stream')
+def stream():
+    """ check message delivery service """
+    return flask.Response(get_message(),
+                          mimetype="text/event-stream")
 
 """ Routes for serving the TestApp """
 
